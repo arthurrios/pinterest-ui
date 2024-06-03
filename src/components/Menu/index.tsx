0a5f51366f86e87449293fb33402d616ad0802d3
arthur <arthur.rios007@gmail.com>
2024-06-03 16:54:46 -0300

    feat: Add menu component

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/display-name */
+import { forwardRef } from 'react'
+import { Text, View } from 'react-native'
+import BottomSheet from '@gorhom/bottom-sheet'
+import { MenuProps } from './menu'
+import { FontAwesome } from '@expo/vector-icons'
+import { styles } from './styles'
+import { theme } from '@/theme'
+import { MenuButton } from '../MenuButton'
+
+export const Menu = forwardRef<BottomSheet, MenuProps>(({ onClose }, ref) => {
+  return (
+    <BottomSheet
+      ref={ref}
+      index={0}
+      snapPoints={[0.01, 230]}
+      backgroundStyle={styles.container}
+      handleComponent={() => null}
+    >
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <FontAwesome
+            name="close"
+            size={24}
+            onPress={onClose}
+            color={theme.colors.white}
+          />
+          <Text style={styles.title}>Start creating now</Text>
+        </View>
+
+        <View style={styles.options}>
+          <MenuButton title="Pin" icon="home" />
+          <MenuButton title="Collage" icon="paste" />
+          <MenuButton title="Folder" icon="folder" />
+        </View>
+      </View>
+    </BottomSheet>
+  )
+})
